c6afc828b307ae2875875820c42213c643029412
AgentBoo <zuzi.han@gmail.com>
2017-08-25 01:12:01 -0400

    changes to mustache file for <button value> vs. before, code only worked for one word todos

--- todo.js
+++ todo.js
@@ -11,31 +11,32 @@ const bodyParser = require('body-parser');
   app.use(bodyParser.json());                             // for parsing application/json
   app.use(bodyParser.urlencoded({extended: true}))        // <true> more complex algorithm for parsing application/x-www-form-urlencoded
 
-const path = require('path');
-  app.use('/public', express.static('public'));
+app.use('/public', express.static('public'));             // express.static() middleware serves all files in the public dir (put static files in public)
 
 // NOTE: Route handlers
 
-let todo = [];
+let todo = ['Poop'];
 let complete = [];
 
-app.get("/", (req, res) => {
+app.get("/", (req, res) => {                                                // render index.mustache upon GET http request to root / + #todo and #complete hashes for mustache
   if(req.body) { res.render('index', { 'todo': todo, 'complete': complete }) };
 });
 
-app.post("/", (req, res) => {
-  if(req.body.todo) { todo.push(req.body.todo) }
+app.post("/", (req, res) => {                                              // all my buttons do POST http request to root /
+  if(req.body.todo) { todo.push(req.body.todo) }                           // if POST request body exists + input field is filled with a TODO text --> add the TODO to a todo array
   else if(req.body.btnMark) { complete.push(todo.splice(todo.findIndex((element) => {
-      if(req.body.btnMark == element){ return element };
+      if(req.body.btnMark == element){ return element };                   // if POST body has empty todo + filled btnMark field --> find index of array item that has btnMark[i] value === todo[i];
     }), 1)); }
   else if(req.body.btnDelete) { todo.splice(todo.findIndex((element) => {
-      if(req.body.btnDelete == element){ return element }
+      if(req.body.btnDelete == element){ return element }                 // if POST body has empty todo + filled btnDelete field --> find index of array item that has btnDelete[i] value === todo[i];
     }), 1); }
   else if(req.body.complDelete) { complete.splice(complete.findIndex((element) => {
-    if(req.body.complDelete == element){ return element };
+    if(req.body.complDelete == element){ return element };                // if POST body has empty todo + filled complDelete field --> find index of array item that has complDelete[i] value === complete[i];
   }), 1); }
   else {};
   res.redirect("/");
 });
 
-app.listen(3000, () => { console.log('BTS : Fire') });
+// NOTE: The above note does not seem to work at all for duplicate TODOs
+
+app.listen(3000, () => { console.log('Blackpink: Boombayah') });
